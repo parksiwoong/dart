ef35f9c320768f052d28bbf7a5c0ffa013336aee
parksiwoong <tldnd5656@naver.com>
2023-02-02 23:12:24 +0900

    Oop

--- bin/dart.dart
+++ bin/dart.dart
@@ -179,7 +179,7 @@ class IDol{
   List<String> members = ['지수', '제니'];
   //생성자 생성법
   IDol(String name, List<String> members)
-    : this.name = name, //this가 뜻하는건 클래스의 name members
+    : this.name = name, //this가 뜻하는건 클래스의 name members , {}대신 : 로 쓸 수 있음
       this.members = members;
 
   IDol.fromList(List values)
@@ -200,4 +200,53 @@ class IDol{
   set firstMember(String name){
       this.members[0] = name;
   }
+
+  /**/
+}
+
+/*************************************************************************************************/
+
+/** 언더스코어 (프라이비트)  _ 는 private 라고 생각하면됨 컴파일하더라도 _Idols은 생성자로 가져오지않으면 읽히지 않음 */
+
+void main9(){
+  _Idols idols = _Idols( //이렇게 가져와서 써야함
+    '블랙핑크'
+  );
+}
+class _Idols{
+  final String name;
+
+  _Idols(this.name);
+  void intraduce(){
+    print('저희맴버는 ${this.name} 입니다. ');
+  }
+}
+
+/*************************************************************************************************/
+/** 상속 */
+void main10(){
+  print('----- Idol ------');
+  Idol apink = Idol(name: '에이핑크', membersCount: 5);
+}
+class Idol{
+  String name;
+  int membersCount;
+
+  Idol({
+   required this.name,
+   required this.membersCount,
+  });
+
+  void sayName(){
+    print('저는 ${this.name} 입니다. ');
+  }
+  void sayMembersCount(){
+    print('${this.name}은 ${this.membersCount}명의 맴버가 있습니다.');
+  }
+}
+// super 는 부모클래스의 변수를 지칭하는것
+class BoyGroup extends Idol{
+  // BoyGroup({required super.name, required super.membersCount}); 상속 두가지 방법중 1
+  BoyGroup(String name, int membersCount,): super(name : name, membersCount: membersCount);
+
 }
