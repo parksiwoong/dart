0e2665fd457a8acb9a03073291b870d337e1534e
parksiwoong <tldnd5656@naver.com>
2023-02-17 22:36:57 +0900

    FunctionalProgramming

--- lib/dart.dart
+++ lib/dart.dart
@@ -1,3 +1,35 @@
 int calculate() {
   return 6 * 7;
 }
+<!DOCTYPE html>
+<!-- JavaScript Bundle with Popper -->
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>accordion</title>
+</head>
+<body>
+<table class="table">
+<thead>
+<tr>
+<th>No</th>
+<th>FAQ</th>
+<th>Data</th>
+<th>Hit</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>1</td>
+<td>FAQ 제목 들어가는 자리</td>
+<td>이용안내</td>
+<td>2032.01.01</td>
+<td>84</td>
+</tr>
+</tbody>
+</table>
+</body>
+</html>
